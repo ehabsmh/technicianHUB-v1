43ac02fc6fad3418fdb8e64e50944725002053d3
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-16 23:23:15 +0300

    Business logic omitted from this file, and now it contains only the routes

--- api/v1/views/auth.js
+++ api/v1/views/auth.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, register } from '../../../Controller/user.js';
+import { login, register } from '../../../Controller/auth.js';
 
 const authRouter = express.Router();
 
