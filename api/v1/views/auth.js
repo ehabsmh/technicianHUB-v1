8ff37d4254d44787ac68f5f7f75437b0305008b8
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-16 23:36:44 +0300

    imports auth controller

--- api/v1/views/auth.js
+++ api/v1/views/auth.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, register } from '../../../Controller/auth.js';
+import { login, register } from '../../../Controllers/auth.js';
 
 const authRouter = express.Router();
 
