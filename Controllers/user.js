8e56f5a53ea0d3d838216c9f8c244b493f13a196
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-17 21:26:13 +0300

    Get single user logic

--- Controllers/user.js
+++ Controllers/user.js
@@ -4,3 +4,13 @@ export const getUsers = async (req, res) => {
     const users = await User.find({ role: 'user' });
     res.json(users);
 }
+
+export const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        console.log(user);
+        res.json(user);
+    } catch (err) {
+        res.json({ "error": "User not found" })
+    }
+}
