65a2345ff46dcb401d2b6bdb023eb549491baa5f
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-17 21:23:27 +0300

    Route for a single user

--- api/v1/views/user.js
+++ api/v1/views/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import User from '../../../models/users.js';
-import { getUsers } from '../../../Controllers/user.js';
+import { getUser, getUsers } from '../../../Controllers/user.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/users', getUsers);
+userRouter.get('/users/:id', getUser);
 
 export default userRouter;
