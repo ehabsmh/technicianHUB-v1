198a5137f0fe1bc3efa5dc635a023397c991445d
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-21 01:55:29 +0300

    user controller for technicians and a specific technician.

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,16 +1,34 @@
 import User from "../models/users.js";
 
-export const getUsers = async (req, res) => {
-    const users = await User.find({ role: 'user' });
-    res.json(users);
+export const getTechnician = async (req, res) => {
+    const techId = req.params.id;
+    const projection = {
+        firstName: 1, lastName: 1, picture: 1, technicianDetails: 1
+    }
+
+    const user = await User.findOne({ _id: techId, role: 'technician' }, projection);
+    if (!user) {
+        return res.status(404).json({ "error": "Technician not found" });
+    }
+
+    res.json(user);
 }
 
-export const getUser = async (req, res) => {
+
+export const getTechniciansByService = async (req, res) => {
+    const service = req.query.service;
+
     try {
-        const user = await User.findById(req.params.id);
-        console.log(user);
-        res.json(user);
-    } catch (err) {
-        res.json({ "error": "User not found" })
+        const technicians = await User.find({
+            'technicianDetails.service': service,
+            'technicianDetails.isAvailable': true
+        });
+        res.json(technicians);
+    } catch (error) {
+        res.status(401).json({ "error": "Failed to get technicians", err: error });
     }
+}
+
+export const getTechnicianProfile = async (req, res) => {
+
 }
