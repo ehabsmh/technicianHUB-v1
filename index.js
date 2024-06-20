3642ae7de8d99fc53f89735b3a9961d6d617f73a
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-21 01:47:00 +0300

    environment variables configuration imported

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import express from 'express'
 import DB from './db/db.js'
 import userRouter from './api/v1/views/user.js';
 import authRouter from './api/v1/views/auth.js';
-import techRouter from './api/v1/views/technician.js';
+// import techRouter from './api/v1/views/technician.js';
 
 export const app = express();
 const port = 3000;
@@ -10,6 +11,6 @@ const db = new DB();
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/", userRouter);
-app.use("/api/v1/", techRouter);
+// app.use("/api/v1/", techRouter);
 
 export const server = app.listen(port, () => console.log(`Technician HUB listening on port ${port}!`));
