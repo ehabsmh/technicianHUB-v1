e056330ff63b4ceb1147afd8b2f2dbfe345c74d2
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-21 01:55:48 +0300

    technician controllers not yet implemented

--- Controllers/technician.js
+++ Controllers/technician.js
@@ -1,20 +1,3 @@
 import User from "../models/users.js";
 
-export const getTechnicians = async (req, res) => {
-    const users = await User.find({ role: 'technician' });
-    res.json(users);
-}
 
-export const getTechnician = async (req, res) => {
-    const techId = req.params.id;
-    const projection = {
-        firstName: 1, lastName: 1, phone: 1, picture: 1, technicianDetails: 1
-    }
-
-    const user = await User.findOne({ _id: techId, role: 'technician' }, projection);
-    if (!user) {
-        return res.status(404).json({ "error": "Technician not found" });
-    }
-
-    res.json(user);
-}
