78b505855c36806ab843b151b39208b5af98f0c2
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-22 02:35:04 +0300

    auth middleware added to all user routes.

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from 'express'
 import db from './db/db.js'
 import userRouter from './api/v1/views/user.js';
 import authRouter from './api/v1/views/auth.js';
-// import techRouter from './api/v1/views/technician.js';
+import { auth } from './middlewares/auth.js';
 
 export const app = express();
 const port = 3000;
 app.use(express.json());
 app.use("/api/v1/auth/", authRouter);
+app.use(auth);
 app.use("/api/v1/users/", userRouter);
-// app.use("/api/v1/", techRouter);
+// app.use("/api/v1/technicians/", techRouter);
 
 export const server = app.listen(port, () => console.log(`Technician HUB listening on port ${port}!`));
