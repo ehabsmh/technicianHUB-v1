5f51b3c80841a7e975dd6828cf6a6660b0b3a1cc
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-22 02:31:19 +0300

    refactored to a user controller class for user routes.

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,34 +1,48 @@
+import db from "../db/db.js";
 import User from "../models/users.js";
 
-export const getTechnician = async (req, res) => {
-    const techId = req.params.id;
+class UserController {
+  static async getTechnician(req, res) {
+    const techId = req.params.techId;
+
     const projection = {
-        firstName: 1, lastName: 1, picture: 1, technicianDetails: 1
+      firstName: 1, lastName: 1, picture: 1, technicianDetails: 1
     }
 
-    const user = await User.findOne({ _id: techId, role: 'technician' }, projection);
-    if (!user) {
-        return res.status(404).json({ "error": "Technician not found" });
+    const technician = await User.findOne({ _id: techId, role: 'technician' }, projection);
+    if (!technician) {
+      return res.status(404).json({ "error": "Technician not found" });
     }
 
-    res.json(user);
-}
+    res.json({ technician });
+  }
 
-
-export const getTechniciansByService = async (req, res) => {
+  static async getTechniciansByService(req, res) {
     const service = req.query.service;
+    if (!service) {
+      return res.status(400).json({
+        "usage": [{
+          purpose: "Get technicians by service",
+          example: "/api/v1/users/technicians?service=Electrician"
+        },
+        {
+          purpose: "Get technician by Id",
+          example: "/api/v1/users/technicians/:techId"
+        }],
+      });
+    }
 
     try {
-        const technicians = await User.find({
-            'technicianDetails.service': service,
-            'technicianDetails.isAvailable': true
-        });
-        res.json(technicians);
+      const technicians = await User.find({
+        'technicianDetails.service': service,
+        'technicianDetails.isAvailable': true
+      });
+
+      res.json({ technicians });
     } catch (error) {
-        res.status(401).json({ "error": "Failed to get technicians", err: error });
+      res.status(401).json({ "error": error });
     }
+  }
 }
 
-export const getTechnicianProfile = async (req, res) => {
-
-}
+export default UserController;
