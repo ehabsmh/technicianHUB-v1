d8249a743e918cf23b89275ea78abe1580b497d0
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-22 23:22:32 +0300

    eslint and documented.

--- api/v1/views/user.js
+++ api/v1/views/user.js
@@ -6,19 +6,31 @@ import JobRequestController from '../../../Controllers/jobRequest.js';
 
 const userRouter = express.Router();
 
-// User view to the technicians
+/* User view to the technicians */
+// Get technicians by service
 userRouter.get('/technicians/', userAuthorizations, UserController.getTechniciansByService);
+
+// Get technician by id
 userRouter.get('/technicians/:techId', userAuthorizations, UserController.getTechnician);
 
-// User view to technicians Reviews
+/* User view to technicians Reviews */
+// Create a user review for a technician
 userRouter.post('/createReview', userAuthorizations,
-    ReviewController.createTechnicianReview);
-userRouter.get('/technicians/:techId/reviews', ReviewController.getTechnicianReviews);
-userRouter.put('/technicians/reviews/:reviewId', userAuthorizations,
-    ReviewController.updateTechnicianReview);
-userRouter.delete('/technicians/reviews/:reviewId', userAuthorizations,
-    ReviewController.deleteTechnicianReview);
+  ReviewController.createTechnicianReview);
 
-// User view to job requests sent to technicians
+// Get technician reviews
+userRouter.get('/technicians/:techId/reviews', ReviewController.getTechnicianReviews);
+
+// Update a user review for a technician
+userRouter.put('/technicians/reviews/:reviewId', userAuthorizations,
+  ReviewController.updateTechnicianReview);
+
+// Delete a user review for a technician
+userRouter.delete('/technicians/reviews/:reviewId', userAuthorizations,
+  ReviewController.deleteTechnicianReview);
+
+/* User view to job requests sent to technicians */
+// Create a job request
 userRouter.post('/jobRequests', userAuthorizations, JobRequestController.createJobRequest);
+
 export default userRouter;
