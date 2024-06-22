7cccba5a60a1944970d645b1720b11ad93c0baee
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-22 20:02:11 +0300

    confirm an already confirmed email is handled.

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -39,13 +39,21 @@ class AuthController {
   static async confirmEmail(req, res) {
     const emailToken = req.headers.token;
     if (!emailToken) {
-      res.status(400).json({ "error": "Token is required" });
+      res.status(400).json({ error: "Token is required" });
     }
 
     try {
       const { userId } = jwt.verify(emailToken, process.env.JWT_CONFIRM_EMAIL_SECRET);
-      const user = await User.findByIdAndUpdate(userId, { emailConfirmed: true });
-      if (!user) return res.status(400).json({ "error": "Invalid token" });
+      const user = await User.findById(userId);
+
+      if (user.emailConfirmed) {
+        return res.status(400).json({ error: "Email already confirmed" });
+      }
+
+      user.updateOne({ emailConfirmed: true });
+
+      if (!user) return res.status(400).json({ error: "Invalid token" });
+
       res.json({ "message": "Email confirmed successfully" });
     } catch (error) {
       res.status(400).json({ error })
