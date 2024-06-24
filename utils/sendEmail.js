7ebccf4c160eb9c7eec8b3da84d3de90a9789f8e
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-25 02:24:58 +0300

    refactoring of getting file path and token expire date omitted.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer";
 import * as fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url';
 import jwt from 'jsonwebtoken';
-
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -13,23 +11,20 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export async function sendEmail(user, content, payload) {
-  const fileName = fileURLToPath(import.meta.url);
-  const dirName = path.dirname(fileName);
+export async function sendEmail(newUser, fileName, payload) {
+  const dirName = path.join(process.cwd(), '/utils');
 
   try {
-    const htmlFilePath = path.resolve(dirName, content);
+    const htmlFilePath = path.resolve(dirName, fileName);
     const htmlContent = await fs.promises.readFile(htmlFilePath, 'utf-8');
     await transporter.sendMail({
       from: '"Technician HUB 👷‍♂️" <ehabsmh3@gmail.com>',
-      to: user.email,
-      subject: `Hello ${user.firstName}!`,
+      to: newUser.email,
+      subject: `Hello ${newUser.firstName}!`,
       html: htmlContent
     });
 
-    return jwt.sign(payload,
-      process.env.JWT_CONFIRM_EMAIL_SECRET,
-      { expiresIn: '1d' });
+    return jwt.sign(payload, process.env.JWT_CONFIRM_EMAIL_SECRET);
 
   } catch (err) {
     console.error("Something went wrong!", err);
