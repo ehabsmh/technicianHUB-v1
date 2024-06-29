10bd3644450261a1b654f302d7dc6d776a24a7f6
Ehab Hussein <ehabsmh3@gmail.com>
2024-06-29 03:01:07 +0300

    technician fetched.

--- frontend/src/pages/Clients/Technician.jsx
+++ frontend/src/pages/Clients/Technician.jsx
@@ -1,6 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Technician() {
+  const [technician, setTechnician] = useState({});
   const { id } = useParams();
-  return <div>Technician!!!...</div>;
+  const getTechnician = async () => {
+    const options = {
+      headers: { token: localStorage.getItem("token") },
+    };
+    try {
+      const { data } = await axios.get(
+        `http://localhost:3000/api/v1/users/technicians/${id}`,
+        options
+      );
+      const { technician } = data;
+      setTechnician(technician);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTechnician();
+  }, []);
+
+  return (
+    <>
+      <div className="container w-4/5 mx-auto"></div>
+    </>
+  );
 }
