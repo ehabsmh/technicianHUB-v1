b1f939fac7e3933004f31481e54d17c8ed8682af
Ehab Hussein <ehabsmh3@gmail.com>
2024-07-01 03:35:26 +0300

    Fixing the background path.

--- frontend/src/pages/Technicians/TechLanding.jsx
+++ frontend/src/pages/Technicians/TechLanding.jsx
@@ -17,7 +17,7 @@ export default function TechLanding() {
                 Get Started
               </button>
               <img
-                src="../assets/images/get_started.png"
+                src="/assets/images/get_started.png"
                 alt="get started"
                 className="w-56 lg:w-auto text-center ml-10"
               />
