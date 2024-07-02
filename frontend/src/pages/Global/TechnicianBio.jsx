9a76d744cfda25df45c28c1c1b8fba9c9f2352f1
Ehab Hussein <ehabsmh3@gmail.com>
2024-07-02 04:41:22 +0300

    Reusing the same Component for Technicians and clients.

--- frontend/src/pages/Global/TechnicianBio.jsx
+++ frontend/src/pages/Global/TechnicianBio.jsx
@@ -1,9 +1,24 @@
 import { useContext } from "react";
 import TechnicianContext from "../../components/Global/Store";
 import Spinner from "../../components/Spinner";
+import AuthContext from "../../components/Global/AuthContext";
 
 export default function TechnicianBio() {
+  const { loggedUser } = useContext(AuthContext);
   const technician = useContext(TechnicianContext);
+
+  if (loggedUser && loggedUser.role === "technician") {
+    if (!loggedUser) {
+      return (
+        <Spinner
+          spinnerColor="#388da8"
+          spinnerSize="30px"
+          spinnerClassName="register-spinner"
+        />
+      );
+    }
+    return <p>{loggedUser.technicianDetails?.bio}</p>;
+  }
 
   if (!technician || !technician._id) {
     return (
