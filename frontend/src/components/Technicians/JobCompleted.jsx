89d6b79be1dda59400a68843e047d82976885025
Ehab Hussein <ehabsmh3@gmail.com>
2024-07-04 18:51:33 +0300

    Made a counter when it finishes, the technician will be navigated to the home page.

--- frontend/src/components/Technicians/JobCompleted.jsx
+++ frontend/src/components/Technicians/JobCompleted.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function JobCompleted({ setBarProgress }) {
+  const navigate = useNavigate();
+  const [count, setCount] = useState(3);
+
   useEffect(() => {
     setBarProgress("before:right-0");
   });
 
   useEffect(() => {
     localStorage.removeItem("jobId");
-  }, []);
+    setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+
+    if (count === 0) navigate("/tech");
+  }, [count]);
 
   return (
     <>
+      <div className="text-center mt-5">
+        <p>
+          Page refreshes in <span>{count}s</span>
+        </p>
+      </div>
       <div className="flex justify-center items-center mt-10 mx-auto">
         <FontAwesomeIcon
           icon={faCircleCheck}
