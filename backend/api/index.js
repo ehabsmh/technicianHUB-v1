bdf912924e842d225f6f3874b2806398f2a3ab87
Ehab Hussein <ehabsmh3@gmail.com>
2024-08-18 15:21:13 +0300

    creating a socket

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,8 +6,11 @@ import userRouter from './v1/views/user.js';
 import authRouter from './v1/views/auth.js';
 import techRouter from './v1/views/technician.js';
 import cors from 'cors'
+import { Server } from 'socket.io'
+
 export const app = express();
 const port = 3000;
+
 app.use(cors())
 app.use(express.json());
 app.use(express.static('uploads'))
@@ -15,4 +18,18 @@ app.use("/api/v1/auth/", authRouter);
 app.use(auth);
 app.use("/api/v1/users/", userRouter);
 app.use("/api/v1/technicians/", techRouter);
-app.listen(port, () => console.log(`Technician HUB listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Technician HUB listening on port ${port}!`));
+
+const io = new Server(server, {
+    cors: "*"
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected!');
+    socket.on('chat message', (msg) => {
+        console.log(msg);
+    })
+    socket.on('disconnect', () => {
+        console.log("Disconnected");
+    })
+})
